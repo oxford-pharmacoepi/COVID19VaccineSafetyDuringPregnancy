c65c453dd16edad63951a6916ece5d6dcfdb3f11
nmercadeb <nmercade147@gmail.com>
2025-07-21 14:40:23 +0100

    chaacterisation planning

--- Study/Analysis/06_BRCharacterisation.R
+++ Study/Analysis/06_BRCharacterisation.R
@@ -2,11 +2,11 @@
 # Variables to add: socioeconomic status, ethnicity, season pregnancy start
 # Pregnancy: previous pregnancy
 # Comorbidities: alchohol, obesity, diabetes, hypertension, asthma, depression/anxiety, epilepsy
-# Medications: omeprazole/antiacids, antihypertensives, diabetes treatment/s, nsaids, opioids, antidepressants, antiepilepsy, corticosteroids
+# Medications: omeprazole/antiacids, diabetes treatment/s, nsaids, opioids, antidepressants, antiepilepsy, corticosteroids
 
 
 # Characteristics outcome ----
 # Variables to add: socioeconomic status, ethnicity, season pregnancy start
 # Pregnancy: previous pregnancy, gestational age at outcome
 # Comorbidities: alchohol, obesity, diabetes, hypertension, asthma, depression/anxiety, epilepsy
-# Medications: omeprazole/antiacids, antihypertensives, diabetes treatment/s, nsaids, opioids, antidepressants, antiepilepsy, corticosteroids
+# Medications: opioids, antidepressants, antiepilepsy, diabetes treatment/s, nsaids, omeprazole/antiacids, corticosteroids
