96913d84f031090be70012babb6d15fe1925593d
nmercadeb <nmercade147@gmail.com>
2025-09-03 16:04:07 +0200

    last update with hds server + local

--- StudyShiny/ShinyObjective1/data/preprocess.R
+++ StudyShiny/ShinyObjective1/data/preprocess.R
@@ -14,29 +14,7 @@ source(file.path(getwd(), "functions.R"))
 result <- omopgenerics::importSummarisedResult(file.path(getwd(), "data")) |>
   omopgenerics::filterSettings(!.data$table_name %in% c("nco", "source_population", "covid", "covid_vaccines", "covid_vaccines_dose", "other_vaccines")) 
 
-data <- prepareResult(result, resultList)ç
-
-mae <- c(
-  "miscarriage", "maternal_death", "postpartum_endometritis", "postpartum_haemorrhage", 
-  "dysfunctional_labour", "eclampsia", "ectopic_pregnancy", "gestational_diabetes",                  
-  "hellp", "preeclampsia", "preterm_labour", "antepartum_haemorrhage"
-)
-x <- data$incidence |> 
-  omopgenerics::splitGroup() |> 
-  dplyr::mutate(
-    outcome_table_name = dplyr::if_else(.data$outcome_cohort_name %in% mae, "MAE", "AESI")
-  ) |>
-  omopgenerics::uniteGroup(c("denominator_cohort_name", "outcome_cohort_name")) 
-data$incidence <- x |>
-  dplyr::select(!"outcome_table_name") |>
-  omopgenerics::newSummarisedResult(
-    settings = omopgenerics::settings(x) |>
-      dplyr::inner_join(
-        x |> dplyr::distinct(.data$result_id, .data$outcome_table_name),
-        by = "result_id"
-      )
-  )
-
+data <- prepareResult(result, resultList)
 values <- getValues(result, resultList)
 
 # edit choices and values of interest
